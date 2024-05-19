922bb19fe7691094b9ed4025316589270bda2f98
gottie29 <67120052+gottie29@users.noreply.github.com>
2024-05-19 17:38:19 +0200

    bar code reading with google books library
    Here I start the loop to scan a barcode and send this barcode to the google books api to read out the author, title and more informatione about the book.
    LED are integrated to show the status of reading

---
+++ barcodewithISBN.py
@@ -0,0 +1,74 @@
+from time import sleep
+import serial
+from serial.tools.hexlify_codec import hex_decode
+import io
+import RPi.GPIO as GPIO
+import urllib.request
+import json
+import textwrap
+
+LED_PIN_ROT = 16
+LED_PIN_GELB = 20
+LED_PIN_GRUEN = 21
+
+item_count=0   #For storing the no. of scanned items
+scode="" #variable that will contain the scan code
+
+
+BAUDRATE= 19200
+DEVICE = '/dev/ttyACM0'
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(LED_PIN_ROT, GPIO.OUT)
+GPIO.setup(LED_PIN_GELB, GPIO.OUT)
+GPIO.setup(LED_PIN_GRUEN, GPIO.OUT)
+
+def readISBN(isbn):
+    base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+
+    with urllib.request.urlopen(base_api_link + isbn) as f:
+        text = f.read()
+
+    decoded_text = text.decode("utf-8")
+    obj = json.loads(decoded_text) # deserializes decoded_text to a Python object
+
+    if not obj:
+        print("ungueltig")
+        return False
+    try:
+        volume_info = obj["items"][0]
+        authors = obj["items"][0]["volumeInfo"]["authors"]
+
+        # displays title, summary, author, domain, page count and language
+        print("Title:", volume_info["volumeInfo"]["title"])
+        #print("\nSubtitle:", volume_info["volumeInfo"]["subtitle"])
+        #print("\nAutor:", volume_info["volumeInfo"]["authors"][0])
+    
+        #print("\nSummary:\n")
+        #print(textwrap.fill(volume_info["searchInfo"]["textSnippet"], width=65))
+        print("Author(s):", ",".join(authors))
+        print("Public Domain:", volume_info["accessInfo"]["publicDomain"])
+        print("Page count:", volume_info["volumeInfo"]["pageCount"])
+        print("Language:", volume_info["volumeInfo"]["language"])
+        print("**********************\n")
+        return True
+    except:
+        print("ungültig")
+        return False
+
+while True:
+    print("Starte lesen")
+    print("************")
+    with serial.Serial(DEVICE, BAUDRATE, serial.EIGHTBITS, serial.PARITY_NONE) as connection:
+        byte = connection.read(13)
+        #print(byte)
+        isbn = byte.decode('utf-8')
+        if (readISBN(isbn) == True):
+            GPIO.output(LED_PIN_GRUEN, GPIO.HIGH)
+        else:     
+            GPIO.output(LED_PIN_ROT, GPIO.HIGH)
+        sleep(2)
+        GPIO.output(LED_PIN_GRUEN, GPIO.LOW)
+        GPIO.output(LED_PIN_ROT, GPIO.LOW)
